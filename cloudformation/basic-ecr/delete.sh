c003643f7106685f0c47fd77810f35feee889ff1
rieckpil <mail@philipriecks.de>
2020-06-22 12:18:43 +0200

    add basic setup for ECR

---
+++ cloudformation/basic-ecr/delete.sh
@@ -0,0 +1,5 @@
+# Turning off the AWS pager so that the CLI doesn't open an editor for each command result
+export AWS_PAGER=""
+
+aws cloudformation delete-stack --stack-name aws101-basic-ecr
+aws cloudformation wait stack-delete-complete --stack-name aws101-basic-ecr
