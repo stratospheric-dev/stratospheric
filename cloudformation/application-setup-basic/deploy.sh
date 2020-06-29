924c998d046d8d3326da6a44d4c6ff10fb555119
rieckpil <mail@philipriecks.de>
2020-06-29 12:26:55 +0200

    deploy new version of application on each build

---
+++ cloudformation/application-setup-basic/deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Using https://ramblingsofasoftwaredevelopermanager.wordpress.com/2019/05/18/a-lighter-way-to-deploy-to-aws-ecs/ as a template
+
+set -eo pipefail
+
+DOCKER_TAG=$1
+DOCKER_IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/aws101:$DOCKER_TAG
+ECS_SERVICE_NAME=aws101-todo-app
+ECS_CLUSTER_NAME=$(aws ecs list-clusters --output text --query 'clusterArns[0]')
+ECS_EXECUTION_ROLE=$(aws iam get-role --role-name aws101-application-network-ECSTaskExecutionRole-1DR8RAZ6ZKYQJ --output text --query 'Role.Arn')
+
+cat ecs_task_template.json | jq --arg img "$DOCKER_IMAGE" --arg role "$ECS_EXECUTION_ROLE" '.containerDefinitions[0].image = $img | .executionRoleArn = $role' > revision.json
+
+REGISTER_RESULT=$(aws ecs register-task-definition --cli-input-json file://./revision.json)
+TASK_ARN=$(echo $REGISTER_RESULT | jq -r .taskDefinition.taskDefinitionArn)
+
+RESULT=$(aws ecs update-service --cluster $ECS_CLUSTER_NAME --service $ECS_SERVICE_NAME --task-definition $TASK_ARN --region $REGION)
+
+echo "Successfully deployed a new version of $ECS_SERVICE_NAME with build number $DOCKER_TAG"
