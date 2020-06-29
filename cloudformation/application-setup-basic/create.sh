e33e95c919a09079b4e6033c9bc0269292d110ee
rieckpil <mail@philipriecks.de>
2020-06-29 11:05:00 +0200

    rework application setup

---
+++ cloudformation/application-setup-basic/create.sh
@@ -0,0 +1,36 @@
+# Turning off the AWS pager so that the CLI doesn't open an editor for each command result
+export AWS_PAGER=""
+
+REGISTRY_NAME=aws101
+
+aws cloudformation create-stack \
+  --stack-name aws101-application-network \
+  --template-body file://network.yml \
+  --capabilities CAPABILITY_IAM
+
+aws cloudformation wait stack-create-complete --stack-name aws101-application-network
+
+aws cloudformation create-stack \
+  --stack-name aws101-container-registry \
+  --template-body file://registry.yml \
+  --parameters \
+      ParameterKey=RegistryName,ParameterValue=$REGISTRY_NAME \
+      ParameterKey=StackName,ParameterValue=aws101-application-network \
+
+aws cloudformation wait stack-create-complete --stack-name aws101-container-registry
+
+aws cloudformation create-stack \
+  --stack-name aws101-application-stack \
+  --template-body file://service.yml \
+  --parameters \
+      ParameterKey=StackName,ParameterValue=aws101-application-network \
+      ParameterKey=ServiceName,ParameterValue=aws101-todo-app \
+      ParameterKey=ImageUrl,ParameterValue=docker.io/reflectoring/aws-hello-world:latest \
+      ParameterKey=ContainerPort,ParameterValue=8080 \
+      ParameterKey=HealthCheckPath,ParameterValue=/hello \
+      ParameterKey=HealthCheckIntervalSeconds,ParameterValue=90
+
+aws cloudformation wait stack-create-complete --stack-name aws101-application-stack
+
+EXTERNAL_URL=$(aws cloudformation describe-stacks --stack-name aws101-application-network --output text --query 'Stacks[0].Outputs[?OutputKey==`ExternalUrl`].OutputValue | [0]')
+echo "You can access your service at $EXTERNAL_URL"
---
+++ cloudformation/application-setup-basic/delete.sh
@@ -0,0 +1,11 @@
+# Turning off the AWS pager so that the CLI doesn't open an editor for each command result
+export AWS_PAGER=""
+
+aws cloudformation delete-stack --stack-name aws101-application-stack
+aws cloudformation wait stack-delete-complete --stack-name aws101-application-stack
+
+aws cloudformation delete-stack --stack-name aws101-container-registry
+aws cloudformation wait stack-delete-complete --stack-name aws101-container-registry
+
+aws cloudformation delete-stack --stack-name aws101-application-network
+aws cloudformation wait stack-delete-complete --stack-name aws101-application-network
