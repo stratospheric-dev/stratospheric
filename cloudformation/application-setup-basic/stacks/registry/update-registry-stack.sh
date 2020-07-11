a7206a4b8abe1168c561bd66963b936eebaf9b1a
Tom Hombergs <tom.hombergs@gmail.com>
2020-07-12 07:51:22 +1000

    Work around AWS CLI issue that returns error on "no update"

--- cloudformation/application-setup-basic/stacks/registry/update-registry-stack.sh
+++ cloudformation/application-setup-basic/stacks/registry/update-registry-stack.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 export AWS_PAGER=""
 
 aws cloudformation update-stack \
@@ -7,7 +6,26 @@ aws cloudformation update-stack \
   --template-body file://registry.yml \
   --capabilities CAPABILITY_IAM \
   --parameters \
-    ParameterKey=RegistryName,ParameterValue=aws101 || true # https://github.com/aws/aws-cli/issues/3625
+    ParameterKey=RegistryName,ParameterValue=aws101 2> update_error
 
+# Sadly, the AWS CLI returns an error when no update is to be performed. But we want to
+# call it a success if the stack is up-to-date. So, we have to work around this by
+# catching and grepping the error output. See https://github.com/aws/aws-cli/issues/3625.
+
+grep "No updates are to be performed" update_error
+NO_UPDATES=$?
+if [ "$NO_UPDATES" -eq 0 ]
+then
+  echo "Stack is up-to-date! Nothing to do."
+  exit 0
+fi
+
+grep "error" update_error
+ERROR=$?
+if [ "$ERROR" -eq 0 ]
+then
+  echo "Error during stack update!"
+  exit 1
+fi
 
 aws cloudformation wait stack-update-complete --stack-name aws101-container-registry
