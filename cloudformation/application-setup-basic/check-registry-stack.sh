c4c4e37a57e3f2489328d39dde1ce4a7394c4c39
Tom Hombergs <tom.hombergs@gmail.com>
2020-07-11 21:13:00 +1000

    Fix exit on error

--- cloudformation/application-setup-basic/check-registry-stack.sh
+++ cloudformation/application-setup-basic/check-registry-stack.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
-set -e
 export AWS_PAGER=""
 
 # creating the registry stack if it's not running already
-./stack-exists.sh "aws101-container-registry" || true
+./stack-exists.sh "aws101-container-registry"
 stack_exists=$?
 if [ "$stack_exists" -eq 0 ]
 then
   echo "Registry stack is already running...nothing to do!"
 else
   echo "Registry stack not running yet. Creating it now..."
-  ./create-registry-stack.sh
+  ./create-registry-stack.sh || exit 1
   echo "Successfully created the registry stack!"
 fi
