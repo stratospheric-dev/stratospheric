5962e71faccf4c7fcca5d61cb5608aa6fef5f144
rieckpil <mail@philipriecks.de>
2020-07-26 18:58:20 +0200

    use default AWS credentials chain to use ECS credentials

--- application/src/main/java/dev/aws101/TodoApplication.java
+++ application/src/main/java/dev/aws101/TodoApplication.java
@@ -1,13 +1,18 @@
 package dev.aws101;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoApplication {
+public class TodoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
 	}
 
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println(System.getenv("AWS_CONTAINER_CREDENTIALS_RELATIVE_URI"));
+  }
 }
