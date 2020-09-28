df6d398e3bd8897ee92e638892cdcedf556024c9
Björn Wilmsmann <bjoern@wilmsmann.de>
2020-09-28 21:53:42 +0200

    #27, CloudFormation stack

--- cloudformation/sns/create.sh
+++ cloudformation/sns/create.sh
@@ -1,21 +1,12 @@
 # Turning off the AWS pager so that the CLI doesn't open an editor for each command result
 export AWS_PAGER=""
 
-EXTERNAL_URL=$(aws cloudformation describe-stacks --stack-name stratospheric-application-network --output text --query 'Stacks[0].Outputs[?OutputKey==`ExternalUrl`].OutputValue')
-APP_URL=https://app.stratospheric.dev
-
 aws cloudformation create-stack \
   --stack-name stratospheric-sns-topic \
-  --template-body file://sns.yml \
-  --parameters \
-      ParameterKey=AuthName,ParameterValue=$AUTH_NAME \
-      ParameterKey=ExternalUrl,ParameterValue=$APP_URL
+  --template-body file://sns.yml
 
-aws cloudformation wait stack-create-complete --stack-name stratospheric-cognito-topic
+aws cloudformation wait stack-create-complete --stack-name stratospheric-sns-topic
 
-USER_POOL_ID=$(aws cloudformation describe-stacks --stack-name stratospheric-cognito-topic --output text --query 'Stacks[0].Outputs[?OutputKey==`UserPoolId`].OutputValue')
-USER_POOL_CLIENT_ID=$(aws cloudformation describe-stacks --stack-name stratospheric-cognito-topic --output text --query 'Stacks[0].Outputs[?OutputKey==`UserPoolClientId`].OutputValue')
+TODO_UPDATES_ARN=$(aws cloudformation describe-stacks --stack-name stratospheric-sns-topic --output text --query 'Stacks[0].Outputs[?OutputKey==`TodoUpdatesARN`].OutputValue')
 
-echo "AWS Cognito UserPool ID:                  " $USER_POOL_ID
-echo "AWS Cognito UserPool client ID:           " $USER_POOL_CLIENT_ID
-echo "AWS Cognito UserPool client secret:       " $USER_POOL_CLIENT_SECRET
+echo "AWS SNS ARN:                  " $TODO_UPDATES_ARN
--- cloudformation/sns/delete.sh
+++ cloudformation/sns/delete.sh
@@ -1,5 +1,4 @@
 # Turning off the AWS pager so that the CLI doesn't open an editor for each command result
 export AWS_PAGER=""
 
-aws cloudformation delete-stack --stack-name aws101-cognito-user-pool
-aws cloudformation wait stack-delete-complete --stack-name aws101-cognito-user-pool
+aws cloudformation delete-stack --stack-name stratospheric-sns-topic
