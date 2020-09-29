bfa9bf3d173efd9106f8c3b7d41275d5ee10c4a6
Björn Wilmsmann <bjoern@wilmsmann.de>
2020-09-29 18:24:08 +0200

    #27, WebSocket client

--- application/src/main/java/dev/aws101/collaboration/PubSubController.java
+++ application/src/main/java/dev/aws101/collaboration/PubSubController.java
@@ -1,10 +1,11 @@
 package dev.aws101.collaboration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.aws.messaging.config.annotation.NotificationMessage;
 import org.springframework.cloud.aws.messaging.config.annotation.NotificationSubject;
 import org.springframework.cloud.aws.messaging.endpoint.NotificationStatus;
 import org.springframework.cloud.aws.messaging.endpoint.annotation.NotificationMessageMapping;
-import org.springframework.cloud.aws.messaging.endpoint.annotation.NotificationSubscriptionMapping;
 import org.springframework.cloud.aws.messaging.endpoint.annotation.NotificationUnsubscribeConfirmationMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -14,15 +15,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("${custom.updates-topic}")
 public class PubSubController {
 
-  @NotificationSubscriptionMapping
+  private static final Logger LOG = LoggerFactory.getLogger(PubSubController.class.getName());
+
+  @NotificationUnsubscribeConfirmationMapping
   public void confirmUnsubscribeMessage(NotificationStatus notificationStatus) {
     notificationStatus.confirmSubscription();
   }
 
   @NotificationMessageMapping
   @SendTo("/topic/todoUpdates")
-  public void receiveNotification(@NotificationMessage String message, @NotificationSubject String subject) {
-    // TODO: Implement method.
+  public String receiveNotification(@NotificationSubject String subject, @NotificationMessage String message) {
+    LOG.info("Todo update received. Subject {}: {}", subject, message);
+
+    return message;
   }
 
   @NotificationUnsubscribeConfirmationMapping
---
+++ application/src/main/resources/static/todo-updates.js
@@ -0,0 +1,22 @@
+let stompClient = null;
+
+function connect() {
+  const socket = new SockJS('/websocket');
+
+  stompClient = Stomp.over(socket);
+  stompClient.connect({}, function (frame) {
+    console.log('Connection to WebSocket endpoint was successful: ' + frame);
+
+    stompClient.subscribe('/topic/todoUpdates', function (message) {
+      console.log(message);
+    });
+  });
+}
+
+function disconnect() {
+  if (stompClient !== null) {
+    stompClient.disconnect();
+  }
+
+  console.log("Disconnected from WebSocket endpoint.");
+}
