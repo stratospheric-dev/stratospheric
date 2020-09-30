24bba32d1a7c0119d2f06c7512a8bd2314cdee83
Björn Wilmsmann <bjoern@wilmsmann.de>
2020-09-30 09:52:43 +0200

    #27, AWS credentials configuration

--- application/src/main/java/dev/aws101/config/AwsConfig.java
+++ application/src/main/java/dev/aws101/config/AwsConfig.java
@@ -1,10 +1,14 @@
 package dev.aws101.config;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderAsyncClientBuilder;
 import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
@@ -14,12 +18,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class AwsConfig {
 
+  @Value("${cloud.aws.credentials.accessKey}")
+  private String accessKey;
+
+  @Value("${cloud.aws.credentials.secretKey}")
+  private String secretKey;
+
+  @Value("${cloud.aws.region.static}")
+  private String region;
+
   @Bean
   @ConditionalOnProperty(value = "custom.security.enabled", havingValue = "true", matchIfMissing = true)
   public AWSCognitoIdentityProvider awsCognitoIdentityProvider(AWSCredentialsProvider awsCredentialsProvider) {
     return AWSCognitoIdentityProviderAsyncClientBuilder.standard()
       .withCredentials(awsCredentialsProvider)
-      .withRegion("eu-central-1")
+      .withRegion(region)
       .build();
   }
 
@@ -31,5 +44,15 @@ public class AwsConfig {
   @Bean
   public NotificationMessagingTemplate notificationMessagingTemplate(AmazonSNS amazonSNS) {
     return new NotificationMessagingTemplate(amazonSNS);
+  }
+
+  @Bean
+  public AmazonSNS amazonSNS() {
+    BasicAWSCredentials awsCredentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+
+    return AmazonSNSClientBuilder
+      .standard()
+      .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+      .build();
   }
 }
