6b31e29ce636b9f7f4f9469308ad6cfdafb7ec75
Philip Riecks <mail@philipriecks.de>
2020-10-26 08:11:15 +0100

    add basic example on how to use auto-configured clients

--- getting-started-with-spring-boot-on-aws/src/main/java/dev/stratospheric/Application.java
+++ getting-started-with-spring-boot-on-aws/src/main/java/dev/stratospheric/Application.java
@@ -1,13 +1,32 @@
 package dev.stratospheric;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication(exclude = ContextInstanceDataAutoConfiguration.class)
 public class Application {
 
+    private final AmazonS3Client amazonS3Client;
+
+    public Application(AmazonS3Client amazonS3Client) {
+        this.amazonS3Client = amazonS3Client;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @EventListener(classes = ApplicationReadyEvent.class)
+    public void onApplicationReadyEvent(ApplicationReadyEvent event) {
+        System.out.println("I am ready!");
+
+        for (Bucket availableBuckets : amazonS3Client.listBuckets()) {
+            System.out.println(availableBuckets.getName());
+        }
     }
 }
