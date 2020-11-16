a57294addfd3c99e27fe98f7a55f948c09f7d432
Philip Riecks <mail@philipriecks.de>
2020-11-16 17:21:12 +0100

    prepare Keycloak as a local OIDC provider

---
+++ application/src/main/java/dev/stratospheric/config/AwsConfig.java
@@ -0,0 +1,23 @@
+package dev.stratospheric.config;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
+import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderAsyncClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("!dev")
+public class AwsConfig {
+
+  @Bean
+  public AWSCognitoIdentityProvider awsCognitoIdentityProvider(@Value("${cloud.aws.region.static}") String region,
+                                                               AWSCredentialsProvider awsCredentialsProvider) {
+    return AWSCognitoIdentityProviderAsyncClientBuilder.standard()
+      .withCredentials(awsCredentialsProvider)
+      .withRegion(region)
+      .build();
+  }
+}
--- application/src/main/java/dev/stratospheric/config/WebSecurityConfig.java
+++ application/src/main/java/dev/stratospheric/config/WebSecurityConfig.java
@@ -1,28 +1,11 @@
 package dev.stratospheric.config;
 
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
-import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderAsyncClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@Profile("!dev")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-  @Bean
-  public AWSCognitoIdentityProvider awsCognitoIdentityProvider(@Value("${cloud.aws.region.static}") String region,
-                                                               AWSCredentialsProvider awsCredentialsProvider) {
-    return AWSCognitoIdentityProviderAsyncClientBuilder.standard()
-      .withCredentials(awsCredentialsProvider)
-      .withRegion(region)
-      .build();
-  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
