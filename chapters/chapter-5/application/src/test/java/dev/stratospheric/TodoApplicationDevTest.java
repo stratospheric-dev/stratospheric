1bdbbab422572284f7f7fc28b653cb611845b48a
Philip Riecks <mail@philipriecks.de>
2020-12-04 08:41:43 +0100

    add basic test to verify chapter version is starting

---
+++ chapters/chapter-5/application/src/test/java/dev/stratospheric/TodoApplicationDevTest.java
@@ -0,0 +1,40 @@
+package dev.stratospheric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.File;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ActiveProfiles("dev")
+@Testcontainers(disabledWithoutDocker = true)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class TodoApplicationDevTest {
+
+  public static DockerComposeContainer<?> environment =
+    new DockerComposeContainer<>(new File("docker-compose.yml"))
+      .withExposedService("postgres_1", 5432, Wait.forListeningPort())
+      .withExposedService("keycloak_1", 8080, Wait.forHttp("/auth").forStatusCode(200)
+        .withStartupTimeout(Duration.ofSeconds(30)));
+
+  static {
+    environment.start();
+  }
+
+  @Autowired
+  private ApplicationContext applicationContext;
+
+  @Test
+  void contextLoads() {
+    assertNotNull(applicationContext);
+  }
+
+}
