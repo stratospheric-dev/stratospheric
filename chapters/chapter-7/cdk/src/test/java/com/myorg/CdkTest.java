8629ca4844d1ae8f1f2eb0db4bfb93c59650e4fb
Tom Hombergs <tom.hombergs@gmail.com>
2020-12-10 06:37:43 +1100

    add CDK app for chapter "First Steps with CDK"

---
+++ chapters/chapter-7/cdk/src/test/java/com/myorg/CdkTest.java
@@ -0,0 +1,29 @@
+package com.myorg;
+
+import software.amazon.awscdk.core.App;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class CdkTest {
+    private final static ObjectMapper JSON =
+        new ObjectMapper().configure(SerializationFeature.INDENT_OUTPUT, true);
+
+    @Test
+    public void testStack() throws IOException {
+        App app = new App();
+        CdkStack stack = new CdkStack(app, "test");
+
+        // synthesize the stack to a CloudFormation template and compare against
+        // a checked-in JSON file.
+        JsonNode actual = JSON.valueToTree(app.synth().getStackArtifact(stack.getArtifactId()).getTemplate());
+        assertEquals(new ObjectMapper().createObjectNode(), actual);
+    }
+
+}
