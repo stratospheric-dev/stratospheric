793460e292a9fe83e9ae9a8ea3a5acc835e3a085
Philip Riecks <mail@philipriecks.de>
2020-12-20 11:38:07 +0100

    replace JUnit 4 reference with JUnit Jupiter

--- chapters/chapter-8/cdk/src/test/java/com/myorg/CdkTest.java
+++ chapters/chapter-8/cdk/src/test/java/com/myorg/CdkTest.java
@@ -1,15 +1,14 @@
 package com.myorg;
 
+import org.junit.jupiter.api.Test;
 import software.amazon.awscdk.core.App;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import org.junit.Test;
 
 import java.io.IOException;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CdkTest {
     private final static ObjectMapper JSON =
