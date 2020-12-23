c2cd6e632111bd930fbb0a6f2d5e9ecfa4f08afd
Philip Riecks <mail@philipriecks.de>
2020-12-23 13:08:11 +0100

    add OIDC initiated client logout
    Currently not supported with AWS Cognito (RFC is in draft), works with Keycloak

--- chapters/chapter-10/application/src/main/java/dev/stratospheric/config/WebSecurityConfig.java
+++ chapters/chapter-10/application/src/main/java/dev/stratospheric/config/WebSecurityConfig.java
@@ -4,9 +4,23 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private final ClientRegistrationRepository clientRegistrationRepository;
+
+  public WebSecurityConfig(ClientRegistrationRepository clientRegistrationRepository) {
+    this.clientRegistrationRepository = clientRegistrationRepository;
+  }
+
+  private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
+    OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+    successHandler.setPostLogoutRedirectUri("{baseUrl}");
+    return successHandler;
+  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -20,6 +34,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       .mvcMatchers("/", "/health", "/register").permitAll()
       .and()
       .logout()
-      .logoutSuccessUrl("/");
+      .logoutSuccessHandler(oidcLogoutSuccessHandler());
   }
 }
