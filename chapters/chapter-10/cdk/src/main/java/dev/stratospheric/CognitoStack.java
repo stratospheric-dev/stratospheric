e50ac662d621607f01a7c812095f4c4c5c45a057
Philip Riecks <mail@philipriecks.de>
2020-12-24 15:12:26 +0100

    basic CloudFormation export of Cognito variables

--- chapters/chapter-10/cdk/src/main/java/dev/stratospheric/CognitoStack.java
+++ chapters/chapter-10/cdk/src/main/java/dev/stratospheric/CognitoStack.java
@@ -43,17 +43,34 @@ public class CognitoStack extends Stack {
       .cognitoDomain(CognitoDomainOptions.builder().domainPrefix("stratospheric-dev").build())
       .build());
 
-    clients.forEach((key, value) -> userPool.addClient(key, UserPoolClientOptions
-      .builder()
-      .userPoolClientName(key)
-      .generateSecret(true)
-      .oAuth(OAuthSettings
+    clients.forEach((key, value) -> {
+
+      UserPoolClient userPoolClient = userPool.addClient(key, UserPoolClientOptions
         .builder()
-        .flows(OAuthFlows.builder().authorizationCodeGrant(true).build())
-        .scopes(List.of(OAuthScope.OPENID, OAuthScope.EMAIL, OAuthScope.PROFILE))
-        .callbackUrls(value)
-        .build())
-      .build()));
+        .userPoolClientName(key)
+        .generateSecret(true)
+        .oAuth(OAuthSettings
+          .builder()
+          .flows(OAuthFlows.builder().authorizationCodeGrant(true).build())
+          .scopes(List.of(OAuthScope.OPENID, OAuthScope.EMAIL, OAuthScope.PROFILE))
+          .callbackUrls(value)
+          .build())
+        .build());
+
+      new CfnOutput(this, "CognitoUserPoolClient" + key + "Id", CfnOutputProps.builder()
+        .value(userPoolClient.getUserPoolClientId())
+        .build());
+      System.out.println(userPoolClient.getUserPoolClientId());
+
+    });
+
+    new CfnOutput(this, "CognitoUserPoolId", CfnOutputProps.builder()
+      .value(userPool.getUserPoolId())
+      .build());
+
+    new CfnOutput(this, "CognitoUserPoolProviderUrl", CfnOutputProps.builder()
+      .value(userPool.getUserPoolProviderUrl())
+      .build());
 
   }
 }
