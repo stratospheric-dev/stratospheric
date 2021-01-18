7ffc291ae1af8fb3a43e32639bd4d6e0f0f85ccf
Tom Hombergs <tom.hombergs@gmail.com>
2021-01-19 09:34:08 +1100

    disable activemq in docker-compose.yml to try to deploy without activeMq

--- application/src/main/java/dev/stratospheric/config/WebSocketConfig.java
+++ application/src/main/java/dev/stratospheric/config/WebSocketConfig.java
@@ -2,12 +2,14 @@ package dev.stratospheric.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-//@Configuration
+@Configuration
+@Profile("dev")
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
