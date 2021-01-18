d6159395472b9e4ea8ca729ffc58ecec7a7beebb
Tom Hombergs <tom.hombergs@gmail.com>
2021-01-19 09:13:38 +1100

    disable WebSocketConfig to avoid application shut down

--- application/src/main/java/dev/stratospheric/config/WebSocketConfig.java
+++ application/src/main/java/dev/stratospheric/config/WebSocketConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@Configuration
+//@Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
