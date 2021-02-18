2cbc074d91c95b6e7c1bab286d418e6100953e77
Philip Riecks <mail@philipriecks.de>
2021-02-18 16:02:56 +0100

    add missing security config for main app

--- application/src/main/java/dev/stratospheric/todoapp/config/WebSecurityConfig.java
+++ application/src/main/java/dev/stratospheric/todoapp/config/WebSecurityConfig.java
@@ -29,6 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       .authorizeRequests()
       .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
       .mvcMatchers("/", "/health", "/register").permitAll()
+      .anyRequest().authenticated()
       .and()
       .logout()
       .logoutSuccessHandler(logoutSuccessHandler);
