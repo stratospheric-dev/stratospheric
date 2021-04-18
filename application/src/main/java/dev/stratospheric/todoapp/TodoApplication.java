f9581ffe414587836876ac569390c65c3fd1c122
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-04-18 19:06:29 +0200

    Debugging DynamoDB autowiring

--- application/src/main/java/dev/stratospheric/todoapp/TodoApplication.java
+++ application/src/main/java/dev/stratospheric/todoapp/TodoApplication.java
@@ -1,29 +1,11 @@
 package dev.stratospheric.todoapp;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
 
 @SpringBootApplication(exclude = ContextInstanceDataAutoConfiguration.class)
 public class TodoApplication {
-
-  private static final Logger LOG = LoggerFactory.getLogger(TodoApplication.class.getName());
-
-  private final AmazonDynamoDB amazonDynamoDB;
-
-  private final String breadcrumbTableName;
-
-  public TodoApplication(
-    AmazonDynamoDB amazonDynamoDB,
-    @Value("${custom.breadcrumb-table-name}") String breadcrumbTableName
-  ) {
-    this.amazonDynamoDB = amazonDynamoDB;
-    this.breadcrumbTableName = breadcrumbTableName;
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(TodoApplication.class, args);
