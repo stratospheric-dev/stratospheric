a2e75ec49f0df3a410bd4f042b4d9d2674ff66c4
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-04-19 01:13:38 +0200

    Debugging DynamoDB autowiring

--- application/src/main/java/dev/stratospheric/todoapp/config/AwsDynamoDBConfig.java
+++ application/src/main/java/dev/stratospheric/todoapp/config/AwsDynamoDBConfig.java
@@ -1,9 +1,17 @@
 package dev.stratospheric.todoapp.config;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AwsDynamoDBConfig {
 
+  @Bean
+  public DynamoDBMapper dynamoDBMapper(AmazonDynamoDB amazonDynamoDB) {
+    return new DynamoDBMapper(amazonDynamoDB, DynamoDBMapperConfig.DEFAULT);
+  }
 }
 
