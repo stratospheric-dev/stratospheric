c69c9f10f8d6f6a6884e5ea1af104e55ceecc95f
Philip Riecks <mail@philipriecks.de>
2021-05-01 11:33:14 +0200

    try visualize basic Cognito metric

--- cdk/src/main/java/dev/stratospheric/todoapp/cdk/MonitoringStack.java
+++ cdk/src/main/java/dev/stratospheric/todoapp/cdk/MonitoringStack.java
@@ -9,9 +9,12 @@ import software.amazon.awscdk.services.cloudwatch.Dashboard;
 import software.amazon.awscdk.services.cloudwatch.DashboardProps;
 import software.amazon.awscdk.services.cloudwatch.GraphWidget;
 import software.amazon.awscdk.services.cloudwatch.LogQueryWidget;
+import software.amazon.awscdk.services.cloudwatch.Metric;
+import software.amazon.awscdk.services.cloudwatch.MetricProps;
 import software.amazon.awscdk.services.cloudwatch.TextWidget;
 
 import java.util.List;
+import java.util.Map;
 
 public class MonitoringStack extends Stack {
 
@@ -29,13 +32,25 @@ public class MonitoringStack extends Stack {
 
     this.applicationEnvironment = applicationEnvironment;
 
+    CognitoStack.CognitoOutputParameters cognitoOutputParameters =
+      CognitoStack.getOutputParametersFromParameterStore(this, applicationEnvironment);
+
     new Dashboard(this, "applicationDashboard", DashboardProps.builder()
       .dashboardName(applicationEnvironment.getApplicationName())
       .widgets(List.of(
         List.of(
           TextWidget.Builder.create().markdown("# Monitoring Dashboard - proudly created with CDK").build(),
-          GraphWidget.Builder.create().title("Number of registrations").build()
-          ),
+          GraphWidget.Builder.create().title("Number of registrations")
+            .left(List.of(new Metric(MetricProps.builder()
+              .namespace("Cognito")
+              .metricName("SignInSuccess")
+              .dimensions(Map.of(
+                "UserPoolClient", cognitoOutputParameters.getUserPoolClientId(),
+                "UserPool", cognitoOutputParameters.getUserPoolId()))
+              .statistic("sum")
+              .build())))
+            .build()
+        ),
         List.of(
           LogQueryWidget.Builder.create().title("Logs").logGroupNames(List.of("staging-todo-app-logs")).queryString(
             "fields @timestamp, @message" +
