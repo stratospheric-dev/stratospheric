9cdd31b97bbdba87ced73b72defa9c0acae541dc
Philip Riecks <mail@philipriecks.de>
2021-05-01 11:17:42 +0200

    try basic Dashboard CDK stack

--- cdk/src/main/java/dev/stratospheric/todoapp/cdk/MonitoringStack.java
+++ cdk/src/main/java/dev/stratospheric/todoapp/cdk/MonitoringStack.java
@@ -1,12 +1,17 @@
 package dev.stratospheric.todoapp.cdk;
 
 import dev.stratospheric.cdk.ApplicationEnvironment;
-import software.amazon.awscdk.core.App;
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Environment;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
-import software.amazon.awscdk.services.sqs.IQueue;
+import software.amazon.awscdk.services.cloudwatch.Dashboard;
+import software.amazon.awscdk.services.cloudwatch.DashboardProps;
+import software.amazon.awscdk.services.cloudwatch.GraphWidget;
+import software.amazon.awscdk.services.cloudwatch.LogQueryWidget;
+import software.amazon.awscdk.services.cloudwatch.TextWidget;
+
+import java.util.List;
 
 public class MonitoringStack extends Stack {
 
@@ -23,5 +28,20 @@ public class MonitoringStack extends Stack {
       .env(awsEnvironment).build());
 
     this.applicationEnvironment = applicationEnvironment;
+
+    new Dashboard(this, "applicationDashboard", DashboardProps.builder()
+      .dashboardName(applicationEnvironment.getApplicationName())
+      .widgets(List.of(
+        List.of(
+          TextWidget.Builder.create().markdown("# Monitoring Dashboard - proudly created with CDK").build(),
+          GraphWidget.Builder.create().title("Number of registrations").build(),
+          LogQueryWidget.Builder.create().title("Logs").logGroupNames(List.of("staging-todo-app-logs")).queryString(
+            "fields @timestamp, @message" +
+              "| sort @timestamp desc" +
+              "| limit 20")
+            .build()
+        )
+      ))
+      .build());
   }
 }
