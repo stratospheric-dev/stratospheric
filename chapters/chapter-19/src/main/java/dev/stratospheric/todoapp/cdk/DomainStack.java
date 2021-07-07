869efac26b6bce8f9e6a8434e5f08d84c2b14bd0
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-07-07 17:20:47 +0200

    Bumped AWS CDK versions; added CDK app for domain configuration.

--- chapters/chapter-19/src/main/java/dev/stratospheric/todoapp/cdk/DomainStack.java
+++ chapters/chapter-19/src/main/java/dev/stratospheric/todoapp/cdk/DomainStack.java
@@ -4,10 +4,9 @@ import dev.stratospheric.cdk.ApplicationEnvironment;
 import dev.stratospheric.cdk.Network;
 import software.amazon.awscdk.core.*;
 import software.amazon.awscdk.services.certificatemanager.DnsValidatedCertificate;
-import software.amazon.awscdk.services.elasticloadbalancingv2.ApplicationLoadBalancer;
-import software.amazon.awscdk.services.elasticloadbalancingv2.ApplicationLoadBalancerLookupOptions;
+import software.amazon.awscdk.services.elasticloadbalancing.LoadBalancer;
+import software.amazon.awscdk.services.elasticloadbalancingv2.*;
 import software.amazon.awscdk.services.route53.*;
-import software.amazon.awscdk.services.route53.patterns.HttpsRedirect;
 import software.amazon.awscdk.services.route53.targets.LoadBalancerTarget;
 
 import java.util.List;
@@ -39,15 +38,46 @@ class DomainStack extends Stack {
       .hostedZone(hostedZone)
       .region(awsEnvironment.getRegion())
       .domainName(applicationDomain)
-      .subjectAlternativeNames(List.of(String.format("www.%s", applicationDomain)))
+      .subjectAlternativeNames(List.of(applicationDomain))
       .build();
 
-    HttpsRedirect webHttpsRedirect = HttpsRedirect.Builder.create(this, "WebHttpsRedirect")
-      .certificate(websiteCertificate)
-      .recordNames(List.of(String.format("www.%s", applicationDomain)))
-      .targetDomain(applicationDomain)
-      .zone(hostedZone)
-      .build();
+    IApplicationLoadBalancer applicationLoadBalancer = ApplicationLoadBalancer.fromLookup(
+      scope,
+      "LoadBalancer",
+      ApplicationLoadBalancerLookupOptions.builder()
+        .loadBalancerArn(networkOutputParameters.getLoadBalancerArn())
+        .build()
+    );
+    ApplicationListener listener = applicationLoadBalancer.addListener(
+      "HttpListener",
+      BaseApplicationListenerProps.builder()
+        .protocol(ApplicationProtocol.HTTP)
+        .port(80)
+        .build()
+    );
+    listener.addAction(
+      "Redirect",
+      AddApplicationActionProps.builder()
+        .action(ListenerAction.redirect(
+          RedirectOptions.builder()
+            .port("443")
+            .build()
+        ))
+        .build()
+    );
+    ApplicationListenerRule applicationListenerRule = new ApplicationListenerRule(
+      this,
+      "HttpListenerRule",
+      ApplicationListenerRuleProps.builder()
+        .listener(listener)
+        .priority(1)
+        .conditions(
+          List.of(
+            ListenerCondition.pathPatterns(List.of("*"))
+          )
+        )
+        .build()
+    );
 
     ARecord aRecord = ARecord.Builder.create(this, "ARecord")
       .recordName(applicationDomain)
