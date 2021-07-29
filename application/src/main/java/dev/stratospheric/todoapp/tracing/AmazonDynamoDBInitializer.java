0a5119eec8d028d4acc5a89121e638b5f9aaec3c
Philip Riecks <mail@philipriecks.de>
2021-07-29 06:18:22 +0200

    add missing @Component annotation for DynamoDB initializer

--- application/src/main/java/dev/stratospheric/todoapp/tracing/AmazonDynamoDBInitializer.java
+++ application/src/main/java/dev/stratospheric/todoapp/tracing/AmazonDynamoDBInitializer.java
@@ -15,7 +15,9 @@ import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AmazonDynamoDBInitializer {
 
   private static final Logger LOG = LoggerFactory.getLogger(AmazonDynamoDBInitializer.class.getName());
