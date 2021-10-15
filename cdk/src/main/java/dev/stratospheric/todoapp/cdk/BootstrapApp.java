3b3c60fb3ca9e866082565f8d277f9703e6fddb0
Philip Riecks <mail@philipriecks.de>
2021-10-15 08:29:27 +0200

    configure account and region in CDK bootstrap app

--- cdk/src/main/java/dev/stratospheric/todoapp/cdk/BootstrapApp.java
+++ cdk/src/main/java/dev/stratospheric/todoapp/cdk/BootstrapApp.java
@@ -1,6 +1,7 @@
 package dev.stratospheric.todoapp.cdk;
 
 import software.amazon.awscdk.core.App;
+import software.amazon.awscdk.core.Environment;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
 
@@ -14,10 +15,26 @@ public class BootstrapApp {
   public static void main(final String[] args) {
     App app = new App();
 
+    String region = (String) app.getNode().tryGetContext("region");
+    Validations.requireNonEmpty(region, "context variable 'region' must not be null");
+
+    String accountId = (String) app.getNode().tryGetContext("accountId");
+    Validations.requireNonEmpty(accountId, "context variable 'accountId' must not be null");
+
+    Environment awsEnvironment = makeEnv(accountId, region);
+
     Stack bootstrapStack = new Stack(app, "Bootstrap", StackProps.builder()
+      .env(awsEnvironment)
       .build());
 
     app.synth();
+  }
+
+  static Environment makeEnv(String account, String region) {
+    return Environment.builder()
+      .account(account)
+      .region(region)
+      .build();
   }
 
 }
