77d859a175d91e1f05f3cba0a1cdfc873f238f08
Philip Riecks <mail@philipriecks.de>
2021-11-02 08:01:33 +0100

    unify CDK context parameters for chapter-12

---
+++ chapters/chapter-12/cdk/src/main/java/dev/stratospheric/todoapp/cdk/BootstrapApp.java
@@ -0,0 +1,40 @@
+package dev.stratospheric.todoapp.cdk;
+
+import software.amazon.awscdk.core.App;
+import software.amazon.awscdk.core.Environment;
+import software.amazon.awscdk.core.Stack;
+import software.amazon.awscdk.core.StackProps;
+
+/**
+ * This is an empty app that we can use for bootstrapping the CDK with the "cdk bootstrap" command.
+ * We could do this with other apps, but this would require us to enter all the parameters
+ * for that app, which is uncool.
+ */
+public class BootstrapApp {
+
+  public static void main(final String[] args) {
+    App app = new App();
+
+    String region = (String) app.getNode().tryGetContext("region");
+    Validations.requireNonEmpty(region, "context variable 'region' must not be null");
+
+    String accountId = (String) app.getNode().tryGetContext("accountId");
+    Validations.requireNonEmpty(accountId, "context variable 'accountId' must not be null");
+
+    Environment awsEnvironment = makeEnv(accountId, region);
+
+    Stack bootstrapStack = new Stack(app, "Bootstrap", StackProps.builder()
+      .env(awsEnvironment)
+      .build());
+
+    app.synth();
+  }
+
+  static Environment makeEnv(String account, String region) {
+    return Environment.builder()
+      .account(account)
+      .region(region)
+      .build();
+  }
+
+}
