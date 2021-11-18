c0512ebdc5262cea6d889717b0abcf564158eb44
Philip Riecks <mail@philipriecks.de>
2021-11-18 10:25:06 +0100

    extract the SSL certificate setup from the DomainStack

---
+++ cdk/src/main/java/dev/stratospheric/todoapp/cdk/CertificateStack.java
@@ -0,0 +1,41 @@
+package dev.stratospheric.todoapp.cdk;
+
+import java.util.List;
+
+import dev.stratospheric.cdk.ApplicationEnvironment;
+import software.amazon.awscdk.core.Construct;
+import software.amazon.awscdk.core.Environment;
+import software.amazon.awscdk.core.Stack;
+import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.certificatemanager.DnsValidatedCertificate;
+import software.amazon.awscdk.services.route53.HostedZone;
+import software.amazon.awscdk.services.route53.HostedZoneProviderProps;
+import software.amazon.awscdk.services.route53.IHostedZone;
+
+public class CertificateStack extends Stack {
+
+  public CertificateStack(
+    final Construct scope,
+    final String id,
+    final Environment awsEnvironment,
+    final ApplicationEnvironment applicationEnvironment,
+    final String applicationDomain,
+    final String hostedZoneDomain) {
+    super(scope, id, StackProps.builder()
+      .stackName(applicationEnvironment.prefix("Certificate"))
+      .env(awsEnvironment).build());
+
+    IHostedZone hostedZone = HostedZone.fromLookup(this, "HostedZone", HostedZoneProviderProps.builder()
+      .domainName(hostedZoneDomain)
+      .build());
+
+    DnsValidatedCertificate websiteCertificate = DnsValidatedCertificate.Builder.create(this, "WebsiteCertificate")
+      .hostedZone(hostedZone)
+      .region(awsEnvironment.getRegion())
+      .domainName(applicationDomain)
+      .subjectAlternativeNames(List.of(applicationDomain))
+      .build();
+
+    System.out.println("SSL Certificate ARN: " + websiteCertificate.getCertificateArn());
+  }
+}
