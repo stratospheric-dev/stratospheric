f9875fa1d08af57bd2f88de67a77cd8a45d04179
thombergs <tom.hombergs@gmail.com>
2021-12-31 06:46:25 +1100

    add explicit path

--- chapters/chapter-1/application/src/main/java/dev/stratospheric/todoapp/IndexController.java
+++ chapters/chapter-1/application/src/main/java/dev/stratospheric/todoapp/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-  @GetMapping
+  @GetMapping(path="/")
   public String getIndex() {
     return "index";
   }
