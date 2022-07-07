bc6bfaf40cdfa01dfd5d2a7a16da74f53fb76bbb
Philip Riecks <mail@philipriecks.de>
2022-07-07 12:45:35 +0200

    finalize tests

--- application/src/test/java/dev/stratospheric/todoapp/DockerComposeEnvironmentVerificationTest.java
+++ application/src/test/java/dev/stratospheric/todoapp/DockerComposeEnvironmentVerificationTest.java
@@ -7,11 +7,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@ActiveProfiles("dev")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DockerComposeEnvironmentVerificationTest {
 
