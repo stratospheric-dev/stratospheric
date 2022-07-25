c9a178a43df3ed74f05390af1e2ddd25c9065077
Philip Riecks <mail@philipriecks.de>
2022-07-25 09:49:49 +0200

    add missing methods described in the manuscript

--- application/src/main/java/dev/stratospheric/todoapp/tracing/TraceDao.java
+++ application/src/main/java/dev/stratospheric/todoapp/tracing/TraceDao.java
@@ -1,8 +1,13 @@
 package dev.stratospheric.todoapp.tracing;
 
 import java.time.ZonedDateTime;
+import java.util.List;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ComparisonOperator;
+import com.amazonaws.services.dynamodbv2.model.Condition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
@@ -31,5 +36,34 @@ public class TraceDao {
     dynamoDBMapper.save(breadcrumb);
 
     LOG.info("Successfully stored breadcrumb trace");
+  }
+
+  public List<Breadcrumb> findAllEventsForUser(String username) {
+    Breadcrumb breadcrumb = new Breadcrumb();
+    breadcrumb.setUsername(username);
+
+    DynamoDBQueryExpression<Breadcrumb> queryExpression =
+      new DynamoDBQueryExpression<Breadcrumb>()
+        .withHashKeyValues(breadcrumb);
+
+    return dynamoDBMapper.query(Breadcrumb.class, queryExpression);
+  }
+
+  public List<Breadcrumb> findUserTraceForLastTwoWeeks(String username) {
+    ZonedDateTime now = ZonedDateTime.now();
+    ZonedDateTime twoWeeksAgo = now.minusWeeks(2);
+    Condition timestampCondition = new Condition()
+      .withComparisonOperator(ComparisonOperator.GT.toString())
+      .withAttributeValueList(new AttributeValue().withS(twoWeeksAgo.toString()));
+
+    Breadcrumb breadcrumb = new Breadcrumb();
+    breadcrumb.setUsername(username);
+
+    DynamoDBQueryExpression<Breadcrumb> queryExpression =
+      new DynamoDBQueryExpression<Breadcrumb>()
+        .withHashKeyValues(breadcrumb)
+        .withRangeKeyCondition("timestamp", timestampCondition);
+
+    return dynamoDBMapper.query(Breadcrumb.class, queryExpression);
   }
 }
