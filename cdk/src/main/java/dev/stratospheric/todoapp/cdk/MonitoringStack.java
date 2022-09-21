4110688d1075aaa1597c50241714adf40e9762e6
Philip Riecks <mail@philipriecks.de>
2022-09-21 16:19:33 +0200

    prepare second dashboard
    DEPLOY NOW

--- cdk/src/main/java/dev/stratospheric/todoapp/cdk/MonitoringStack.java
+++ cdk/src/main/java/dev/stratospheric/todoapp/cdk/MonitoringStack.java
@@ -57,6 +57,7 @@ public class MonitoringStack extends Stack {
         cognitoOutputParameters.getUserPoolId()
       ));
 
+
     new OperationalCloudWatchDashboard(this, "operationalCloudWatchDashboard", applicationEnvironment,
       awsEnvironment,
       new OperationalCloudWatchDashboard.InputParameter(
