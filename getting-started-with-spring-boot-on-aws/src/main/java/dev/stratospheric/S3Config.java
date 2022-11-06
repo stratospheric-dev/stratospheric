5d5ccc3e7d627c1923410015f2a74d56ae922524
Björn Wilmsmann <bjoern@bjoernkw.com>
2022-11-06 18:25:02 +0100

    Spring Boot 3

--- getting-started-with-spring-boot-on-aws/src/main/java/dev/stratospheric/S3Config.java
+++ getting-started-with-spring-boot-on-aws/src/main/java/dev/stratospheric/S3Config.java
@@ -4,7 +4,6 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.services.sqs.SqsClient;
 
 @Configuration
 public class S3Config {
