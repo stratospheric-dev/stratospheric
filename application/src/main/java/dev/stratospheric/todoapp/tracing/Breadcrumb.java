643a0e45ed1387488eb5bd93100e885d6b264598
Björn Wilmsmann <bjoern@bjoernkw.com>
2023-03-26 01:29:17 +0100

    Update to Spring Cloud 3.0.0-RC2

--- application/src/main/java/dev/stratospheric/todoapp/tracing/Breadcrumb.java
+++ application/src/main/java/dev/stratospheric/todoapp/tracing/Breadcrumb.java
@@ -1,6 +1,5 @@
 package dev.stratospheric.todoapp.tracing;
 
-
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
