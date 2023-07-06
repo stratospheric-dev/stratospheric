82af9a7868422cde55b08d10e59afdb77da374c1
Philip Riecks <mail@philipriecks.de>
2023-07-06 10:06:54 +0200

    simplify Websocket connection
    Fallback to Spring Boot's autoconfiguration

--- application/src/main/java/dev/stratospheric/todoapp/config/CustomConfigurationProperties.java
+++ application/src/main/java/dev/stratospheric/todoapp/config/CustomConfigurationProperties.java
@@ -1,7 +1,6 @@
 package dev.stratospheric.todoapp.config;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
@@ -20,8 +19,7 @@ class CustomConfigurationProperties {
   @NotEmpty
   private String externalUrl;
 
-  @NotNull
-  private Boolean autoConfirmCollaborations;
+  private boolean autoConfirmCollaborations;
 
   @NotEmpty
   private String environment;
@@ -29,23 +27,11 @@ class CustomConfigurationProperties {
   @NotEmpty
   private String confirmEmailFromAddress;
 
-  @NotEmpty
-  private String webSocketRelayEndpoint;
+  private boolean webSocketRelayUseSsl;
 
-  @NotEmpty
-  private String webSocketRelayUsername;
+  private boolean useCognitoAsIdentityProvider;
 
-  @NotEmpty
-  private String webSocketRelayPassword;
-
-  @NotNull
-  private Boolean webSocketRelayUseSsl;
-
-  @NotNull
-  private Boolean useCognitoAsIdentityProvider;
-
-  @NotNull
-  private Boolean provideTestTodoController;
+  private boolean provideTestTodoController;
 
   public Set<String> getInvitationCodes() {
     return invitationCodes;
@@ -93,30 +79,6 @@ class CustomConfigurationProperties {
 
   public void setConfirmEmailFromAddress(String confirmEmailFromAddress) {
     this.confirmEmailFromAddress = confirmEmailFromAddress;
-  }
-
-  public String getWebSocketRelayEndpoint() {
-    return webSocketRelayEndpoint;
-  }
-
-  public void setWebSocketRelayEndpoint(String webSocketRelayEndpoint) {
-    this.webSocketRelayEndpoint = webSocketRelayEndpoint;
-  }
-
-  public String getWebSocketRelayUsername() {
-    return webSocketRelayUsername;
-  }
-
-  public void setWebSocketRelayUsername(String webSocketRelayUsername) {
-    this.webSocketRelayUsername = webSocketRelayUsername;
-  }
-
-  public String getWebSocketRelayPassword() {
-    return webSocketRelayPassword;
-  }
-
-  public void setWebSocketRelayPassword(String webSocketRelayPassword) {
-    this.webSocketRelayPassword = webSocketRelayPassword;
   }
 
   public Boolean getWebSocketRelayUseSsl() {
