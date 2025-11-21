4a1a63283e5490660d48a73f18c00b207250ddc6
Björn Wilmsmann <bjoern@bjoernkw.com>
2025-11-21 15:28:55 +0100

    Reverting to Spring Boot 3.5.x for now.

--- application/src/main/java/dev/stratospheric/todoapp/config/WebSecurityConfig.java
+++ application/src/main/java/dev/stratospheric/todoapp/config/WebSecurityConfig.java
@@ -1,8 +1,8 @@
 package dev.stratospheric.todoapp.config;
 
-import org.springframework.boot.health.actuate.endpoint.HealthEndpoint;
-import org.springframework.boot.security.autoconfigure.actuate.web.servlet.EndpointRequest;
-import org.springframework.boot.security.autoconfigure.web.servlet.PathRequest;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
