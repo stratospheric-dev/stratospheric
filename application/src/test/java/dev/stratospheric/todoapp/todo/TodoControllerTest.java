f2f5b760eb85c0f3c113416e6442b221ff1de28f
Björn Wilmsmann <bjoern@bjoernkw.com>
2025-11-22 00:19:47 +0100

    Tentative upgrade to Spring Boot 4

--- application/src/test/java/dev/stratospheric/todoapp/todo/TodoControllerTest.java
+++ application/src/test/java/dev/stratospheric/todoapp/todo/TodoControllerTest.java
@@ -7,7 +7,7 @@ import dev.stratospheric.todoapp.util.TestGitProperties;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
