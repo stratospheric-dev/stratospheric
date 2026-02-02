0b6093ed355cd4f1333ed9e5716c7765154a582f
Björn Wilmsmann <bjoern@bjoernkw.com>
2026-02-02 23:16:43 +0100

    Unused import

--- application/src/main/java/dev/stratospheric/todoapp/config/WebSecurityConfig.java
+++ application/src/main/java/dev/stratospheric/todoapp/config/WebSecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.boot.security.autoconfigure.web.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
