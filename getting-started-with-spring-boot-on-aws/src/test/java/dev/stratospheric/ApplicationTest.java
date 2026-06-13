9ce9f107f395e24e3eb54edfeeab7bf91378fe36
Björn Wilmsmann <bjoern@bjoernkw.com>
2026-06-13 15:08:57 +0200

    Spring Boot update

--- getting-started-with-spring-boot-on-aws/src/test/java/dev/stratospheric/ApplicationTest.java
+++ getting-started-with-spring-boot-on-aws/src/test/java/dev/stratospheric/ApplicationTest.java
@@ -9,16 +9,13 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.web.client.RestClient;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.localstack.LocalStackContainer;
 import org.testcontainers.utility.DockerImageName;
 
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static org.testcontainers.containers.localstack.LocalStackContainer.Service.S3;
-import static org.testcontainers.containers.localstack.LocalStackContainer.Service.SQS;
-import static org.testcontainers.containers.localstack.LocalStackContainer.Service.SSM;
 
 @Testcontainers
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -35,7 +32,7 @@ class ApplicationTest {
   @Container
   static LocalStackContainer localStack =
     new LocalStackContainer(DockerImageName.parse("localstack/localstack:4.7.0"))
-      .withServices(SQS, S3, SSM);
+      .withServices("sqs", "s3", "ssm");
 
   @BeforeAll
   static void beforeAll() throws IOException, InterruptedException {
@@ -47,7 +44,7 @@ class ApplicationTest {
   static void overrideConfiguration(DynamicPropertyRegistry registry) {
     registry.add("custom.bucket-name", () -> SAMPLE_BUCKET);
     registry.add("custom.sqs-queue-name", () -> SAMPLE_QUEUE);
-    registry.add("spring.cloud.aws.endpoint", () -> localStack.getEndpointOverride(S3));
+    registry.add("spring.cloud.aws.endpoint", () -> localStack.getEndpoint());
     registry.add("spring.cloud.aws.region.static", localStack::getRegion);
     registry.add("spring.cloud.aws.credentials.access-key", localStack::getAccessKey);
     registry.add("spring.cloud.aws.credentials.secret-key", localStack::getSecretKey);
